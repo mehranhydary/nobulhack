e2bc13dda9c1fc9242150c362669fe53f6e24617
Mehran Hydary <mehran.hydary@gmail.com>
2018-06-17 01:10:12 -0400

    transfer from function with test in place

--- contracts/CCCoin.sol
+++ contracts/CCCoin.sol
@@ -10,8 +10,15 @@ contract CCCoin {
         address indexed _to,
         uint256 _value
     );
+    // approve
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     mapping(address => uint256) public balanceOf;
-    
+    mapping(address => mapping(address => uint256)) public allowance;
+
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
@@ -21,6 +28,21 @@ contract CCCoin {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approve(msg.sender, _spender, _value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
