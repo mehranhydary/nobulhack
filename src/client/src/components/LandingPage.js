c08850e0c130648842d62ece75c92252ff189080
Samuel <samuelwlai@gmail.com>
2018-06-23 18:01:08 -0400

    materialize table

--- src/client/src/components/LandingPage.js
+++ src/client/src/components/LandingPage.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 const LandingPage = () => (
     <div>
       <h2>LandingPage</h2>
+      <div class="alert alert-primary" role="alert">
+            This is a primary alert—check it out!
     </div>
+
+    </div>
+  
   );
 
   export default LandingPage
