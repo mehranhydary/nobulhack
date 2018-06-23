c4db7a04fb78150a11172e78c4515323ff518185
Samuel <samuelwlai@gmail.com>
2018-06-23 17:06:20 -0400

    routes

---
+++ src/client/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Dashboard = () => (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+
+export default Dashboard
---
+++ src/client/src/components/Survey.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Survey = () => (
+    <div>
+      <h2>Survey</h2>
+    </div>
+  );
+
+  export default Survey
