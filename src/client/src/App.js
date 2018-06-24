84878860cab5b26abd57c6fab63f726ac54fd680
Mehran Hydary <mehranhydary@gmail.com>
2018-06-23 21:01:46 -0400

    nav, token sale

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-// import Button from '@material-ui/core/Button';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Dashboard from './components/Dashboard'
 import LandingPage from './components/LandingPage'
 import Survey from './components/Survey'
+import PlatformAccess from './components/PlatformAccess'
 
 
 
@@ -14,23 +13,38 @@ class App extends Component {
     return (
       <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">LandingPage</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/survey">Survey</Link>
-          </li>
-        </ul>
-  
-        <hr />
-  
+      <div className="card card-nav-tabs card-plain">
+        <div className="card-header card-header-info">
+          <div className="nav-tabs-navigation">
+              <div className="nav-tabs-wrapper">
+                <ul className="nav justify-content-end nav-tabs">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      <i className="material-icons">battery_charging_full</i>
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/survey">
+                      <i className="material-icons">insert_emoticon</i>
+                      Agent
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/dashboard">
+                      <i className="material-icons">dashboard</i>
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         <Route exact path="/" component={LandingPage} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/survey" component={Survey} />
+        <Route path="/sale" component={PlatformAccess} />
       </div>
     </Router>
     );
